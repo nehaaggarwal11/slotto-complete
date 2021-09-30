d85f658d729e25452e042f882a2b5346516747a6
Gursharan Singh <gursharan.smartdesignhut@gmail.com>
2021-09-30 17:55:49 +0530

    add no in filesystem

--- no/config/filesystems.php
+++ no/config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'no/storage',
+            'url' => env('APP_URL').'/no/storage',
             'visibility' => 'public',
         ],
 
